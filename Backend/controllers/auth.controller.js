1c4db1e433f1cdcb08151e312c5731c8a09d643e
RumanaFarheen1230 <farheenrumana6@gmail.com>
2024-06-03 22:53:38 +0530

    1st commit

---
+++ Backend/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+export const signup = async (req, res) => {
+    try{
+        const {fullName, username, password, confirmpassword, gender} = req.body;
+    }catch (error){
+
+    }
+};
+   export const login = (req, res) => {
+    console.log("loginUser");
+   }
+   export const logout = (req, res) => {
+    console.log("logoutUser");
+   }
+ 
+ 
+
+
+
