94ebcb7293fd489948ec8f514f7540475086978c
Abi-Srinivas <srinivas123a1@gmail.com>
2024-06-08 19:16:51 +0530

    server running...

--- Backend/models/message.model.js
+++ Backend/models/message.model.js
@@ -1,25 +1,24 @@
 import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema(
-    {
+const messageSchema = new mongoose.Schema({
     senderId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: true
     },
-    receiverId:{
+    receiverId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: true
     },
-    message:{
-        type: String,
-        required: true
-    }
-    //createdAt, updatedAt => message.createdAt : 15:30
-},
- {timestamps: true}
-);
+
+    message: {
+        type: "string",
+        required: true,
+
+    },
+    //
+}, { timestamps: true });
 
 const Message = mongoose.model("Message", messageSchema);
 
