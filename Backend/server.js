cb5a2698c4d43f4df71440c6245c24a96a54037a
RumanaFarheen1230 <farheenrumana6@gmail.com>
2024-06-08 12:24:15 +0530

    backend complete

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from"./routes/message.routes.js";
+import userRoutes from"./routes/user.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 dotenv.config();
-const PORT = proces.env.PORT || 5000;
 
-app.get("/",(req, res) => {
+app.use(express.json());//to parse the incoming requuests with JSON payloads (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes);
+
+//app.get("/",(req, res) => {app.use("/api/auth",authRoutes);
     //root route http://localhost:5000/
-    res.send("Hello World!!");
-});
+  //  res.send("Hello World!!");
+//});
 
-app.use("/api/auth",authRoutes)
-app.listen(PORT,() => console.log('Server Running on port ${PORT}')); 
+
+
+app.listen(PORT,() => {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`); 
+});
 
 
